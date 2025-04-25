5060f737d54bf8ef96b19a16f940ca78cb846ac3
James Maroko <jamiemaroko41@gmail.com>
2025-04-25 11:26:03 +0300

    Define SQLModel models and Pydantic schemas for Client, Program, and Enrollment

---
+++ app/models/models.py
@@ -0,0 +1,30 @@
+from typing import Optional, List
+from sqlmodel import Field, SQLModel, Relationship
+
+
+class Enrollment(SQLModel, table=True):
+    client_id: Optional[int] = Field(default=None, foreign_key="client.id", primary_key=True)
+    program_id: Optional[int] = Field(default=None, foreign_key="healthprogram.id", primary_key=True)
+
+
+class HealthProgram(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    description: Optional[str] = None
+
+    enrolled_clients: List["Client"] = Relationship(
+        back_populates="enrolled_programs",
+        link_model=Enrollment
+    )
+
+
+class Client(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    age: int
+    contact: str
+
+    enrolled_programs: List[HealthProgram] = Relationship(
+        back_populates="enrolled_clients",
+        link_model=Enrollment
+    )
---
+++ app/schemas/schemas.py
@@ -0,0 +1,43 @@
+from typing import Optional, List
+from pydantic import BaseModel
+
+class ProgramBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+
+class ProgramCreate(ProgramBase):
+    pass
+
+
+class ProgramRead(ProgramBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class ClientBase(BaseModel):
+    name: str
+    age: int
+    contact: str
+
+
+
+class ClientCreate(ClientBase):
+    pass
+
+class ClientRead(ClientBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class ClientProfile(ClientRead):
+    programs: List[ProgramRead] = []
+
+
+class EnrollRequest(BaseModel):
+    client_id: int
+    program_ids: List[int]
